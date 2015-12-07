ad43554bc4de0d81124ac7daa25262a8ad06d277
Sandeep Raghuraman <sandy.8925@gmail.com>
2015-12-06 21:46:20 -0800

    Change tutorial2 to use Element Buffer Objects which store indices into the actual vertex array - helps in eliminating unnecessary duplicate vertex specification

--- tutorial2.cpp
+++ tutorial2.cpp
@@ -5,9 +5,15 @@
 using namespace std;
 
 GLfloat vertices[] = {
-    -0.5f, -0.5f, 0.0f,
-     0.5f, -0.5f, 0.0f,
-     0.0f,  0.5f, 0.0f
+     0.5f,  0.5f, 0.0f,  // Top Right
+     0.5f, -0.5f, 0.0f,  // Bottom Right
+    -0.5f, -0.5f, 0.0f,  // Bottom Left
+    -0.5f,  0.5f, 0.0f   // Top Left 
+};
+
+GLuint indices[] = {  // Note that we start from 0!
+    0, 1, 3,   // First Triangle
+    1, 2, 3    // Second Triangle
 };  
 
 GLuint VAO;
@@ -76,22 +82,26 @@ void drawStuff() {
 	glClear(GL_COLOR_BUFFER_BIT);
         
         glUseProgram(shaderProgram);
-
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glBindVertexArray(0);
 }
 
 void setupOpenGLDrawingCode() {
-    
-        GLuint VBO;
+        GLuint VBO, EBO;
         glGenBuffers(1, &VBO);
+        glGenBuffers(1, &EBO);
         glGenVertexArrays(1, &VAO);        
         
         glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+        
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+        
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
         
